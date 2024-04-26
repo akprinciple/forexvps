c8060275527894253dd794d3868ba3db5e267b4c
akprinciple <akeemolayiwola9@gmail.com>
2024-04-26 08:07:23 +0100

    New commit

--- dashboard/api/inc/email.php
+++ dashboard/api/inc/email.php
@@ -1,6 +1,6 @@
 <?php
 $smtp = 'smtp.gmail.com';    //Set the SMTP server to send through
-$smtp_username = 'akeemolayiwola9@gmail.com';         //SMTP username
-$smtp_password = 'eqzfcgbiuftjfbvv';                 //SMTP password
-$support_mail = 'leotechworld01@gmail.com';         //Mail to receive complaints from users
+$smtp_username = '';         //SMTP username
+$smtp_password = '';                 //SMTP password
+$support_mail = '';         //Mail to receive complaints from users
 ?>
